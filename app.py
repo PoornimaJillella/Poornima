b8af4976939770c2910056468e21097bcdef6e76
PoornimaJillella <159864520+PoornimaJillella@users.noreply.github.com>
2024-12-10 12:12:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -186,12 +186,10 @@ elif app_mode == "Prediction":
 
 elif app_mode == "About":
     st.title("About Skin Cancer Vision")
-    st.write("""
-        Skin Cancer Vision is a machine learning-based application that uses image analysis and statistical insights
+
+    st.subheader("Skin Cancer Vision is a machine learning-based application that uses image analysis and statistical insights
         to detect potential risk of skin cancer diseases. It provides predictive insights based on uploaded image features
-        or trained model data for diagnosis purposes.
-        """)
-    st.subheader("Our mission is to improve early detection of skin cancer, saving lives through accessible, data-driven insights. We believe that by providing easy-to-use tools powered by AI, we can help users make informed, proactive health decisions.Early detection saves lives. Let’s make it possible—together.")
+        or trained model data for diagnosis purposes.Our mission is to improve early detection of skin cancer, saving lives through accessible, data-driven insights. We believe that by providing easy-to-use tools powered by AI, we can help users make informed, proactive health decisions.Early detection saves lives. Let’s make it possible—together.")
 
 
 
