6587e4a5c347379f17ea50d8127cb91ab0974ba7
PoornimaJillella <159864520+PoornimaJillella@users.noreply.github.com>
2024-12-10 12:08:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -190,9 +190,11 @@ elif app_mode == "About":
         Skin Cancer Vision is a machine learning-based application that uses image analysis and statistical insights
         to detect potential risk of skin cancer diseases. It provides predictive insights based on uploaded image features
         or trained model data for diagnosis purposes.
-        *Our mission is to improve early detection of skin cancer, saving lives through accessible, data-driven insights. We believe that by providing easy-to-use tools powered by AI, we can help users make informed, proactive health decisions.Early detection saves lives. Let’s make it possible—together.
         """)
-    
+    st.subheader("Our mission is to improve early detection of skin cancer, saving lives through accessible, data-driven insights. We believe that by providing easy-to-use tools powered by AI, we can help users make informed, proactive health decisions.
+
+Early detection saves lives. Let’s make it possible—together.
+")
 
 
 
